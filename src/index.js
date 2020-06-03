7b96d438be5aaea9a6a72871cc09886eef845a52
Eduardo <eduardo.freitas.goulart@gmail.com>
2020-06-02 23:39:58 -0300

    first commit frontend

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // arvore de elementos do browser
+import App from './App';
+import './global.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
